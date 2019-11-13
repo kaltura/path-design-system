e8fcdef0d518b723c08ba701fceefed4525cb58f
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-11-13 12:51:51 +0200

    better styling logic

--- packages/ui-inputs/src/button.tsx
+++ packages/ui-inputs/src/button.tsx
@@ -14,62 +14,68 @@ export interface ButtonProps {
     isCTA?: boolean;
 }
 
-// const getUseStyles = (props: any) => createUseStyles<string>((theme: any) => ({
-//     btn: {
-//         color: theme.colorPrimary,
-//         border: props.borderless ? 'none' : '1px solid red'
-//     },
-//     label: {
-//         fontWeight: 'bold'
-//     }
-// }));
-
+/*
 const useStyles = createUseStyles({
-    'btn': {
-        color: (themeWithProps: any) => themeWithProps.isCTA ? '#ffffff' : '#434a4b',
-        backgroundColor: (themeWithProps: any) => themeWithProps.isCTA ? (themeWithProps.isActive ? '#004e5a' : '#008297') : (themeWithProps.isActive ? themeWithProps.colors.greyscale5 : '#ffffff'),
+    btn: {
+        '&:active': (props: any) => props.isCTA? {} : {}
+    },
+    'btn-default': {
+        '&:active': {}
+    },
+    'btn-cta': {
+        '&:active': {}
+    },
+    'btn-active': {
+    },
+    'btn-borderless': {
+        '&:active': {}
+    }
+});
+*/
+const useStyles = createUseStyles({
+    'btn': (props: ButtonProps & {theme: any}) => ({
+        color:  props.isCTA ? '#ffffff' : '#434a4b',
+        backgroundColor:  props.isCTA ? (props.isActive ? '#004e5a' : '#008297') : (props.isActive ? props.theme.colors.greyscale5 : '#ffffff'),
         boxShadow: 'none',
-        fontFamily: (themeWithProps: any) => themeWithProps.button.fontFamily,
-        fontSize: (themeWithProps: any) => themeWithProps.button.fontSize,
-        fontWeight: (themeWithProps: any) => themeWithProps.button.fontWeight,
-        border: (themeWithProps: any) => themeWithProps.borderless || themeWithProps.isCTA ? 'none' : `1px solid ${themeWithProps.colors.greyscale4}`,
-        borderRadius: (themeWithProps: any) => themeWithProps.button.borderRadius,
+        fontFamily:  props.theme.button.fontFamily,
+        fontSize:  props.theme.button.fontSize,
+        fontWeight:  props.theme.button.fontWeight,
+        border:  props.borderless || props.isCTA ? 'none' : `1px solid ${props.theme.colors.greyscale4}`,
+        borderRadius:  props.theme.button.borderRadius,
         '--antd-wave-shadow-color': '#ffffff',
         '&:hover': {
             boxShadow: 'none',
-            color: (themeWithProps: any) => themeWithProps.isCTA ? '#ffffff' : '#434a4b',
-            backgroundColor: (themeWithProps: any) => themeWithProps.isCTA ? (themeWithProps.isActive ? '#004e5a' : '#006879') : (themeWithProps.isActive ? themeWithProps.colors.greyscale5 : themeWithProps.colors.greyscale4),
-            border: (themeWithProps: any) => themeWithProps.borderless || themeWithProps.isCTA ? 'none' : `1px solid ${themeWithProps.colors.greyscale4}`,
+            color:  props.isCTA ? '#ffffff' : '#434a4b',
+            backgroundColor:  props.isCTA ? (props.isActive ? '#004e5a' : '#006879') : (props.isActive ? props.theme.colors.greyscale5 : props.theme.colors.greyscale4),
+            border:  props.borderless || props.isCTA ? 'none' : `1px solid ${props.theme.colors.greyscale4}`,
+        },
+        '&:focus': {
+            backgroundColor:  props.isCTA ? (props.isActive ? '#004e5a' : '#008297') : (props.isActive ? props.theme.colors.greyscale5 : '#ffffff'),
+            border:  props.borderless || props.isCTA ? 'none' : `1px solid ${props.theme.colors.greyscale4}`,
+            color:  props.isCTA ? '#ffffff' : '#434a4b',
         },
         '&:active': {
             boxShadow: 'none',
-            color: (themeWithProps: any) => themeWithProps.isCTA ? '#ffffff' : '#434a4b',
-            backgroundColor: (themeWithProps: any) => themeWithProps.isCTA ? '#004e5a' : themeWithProps.colors.greyscale5,
-            border: (themeWithProps: any) => themeWithProps.borderless || themeWithProps.isCTA ? 'none' : `1px solid ${themeWithProps.colors.greyscale4}`,
-        },
-        '&:focus': {
-            boxShadow: 'none',
-            backgroundColor: (themeWithProps: any) => themeWithProps.isCTA ? '#004e5a' : themeWithProps.colors.greyscale5,
-            color: (themeWithProps: any) => themeWithProps.isCTA ? '#ffffff' : '#434a4b',
-            border: (themeWithProps: any) => themeWithProps.borderless || themeWithProps.isCTA ? 'none' : `1px solid ${themeWithProps.colors.greyscale4}`,
+            color:  props.isCTA ? '#ffffff' : '#434a4b',
+            backgroundColor:  props.isCTA ? '#004e5a' : props.theme.colors.greyscale5,
+            border:  props.borderless || props.isCTA ? 'none' : `1px solid ${props.theme.colors.greyscale4}`,
         },
         '&:disabled': {
             boxShadow: 'none',
             backgroundColor: '#ffffff',
-            border: (themeWithProps: any) => themeWithProps.borderless ? 'none' : `1px solid ${themeWithProps.colors.greyscale4}`,
-            color: (themeWithProps: any) => themeWithProps.colors.greyscale4
+            border:  props.borderless ? 'none' : `1px solid ${props.theme.colors.greyscale4}`,
+            color:  props.theme.colors.greyscale4
         },
         '&:disabled:hover': {
             boxShadow: 'none',
             backgroundColor: '#ffffff'
         }
-    }
+    })
 });
 
 export function Button(props: ButtonProps) {
     const theme = useTheme();
-    // const classes = getUseStyles(props)(theme);
-    const classes = useStyles({...props, ...theme});
+    const classes = useStyles({...props, theme});
     const {label, disabled, onClick} = props;
     return (
         <AntButton className={classes.btn} disabled={disabled} onClick={onClick}>{label}</AntButton>
