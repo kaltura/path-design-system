a9d6b81fd5bf79b6eea531c1d40651605f46e141
Stas <staskgt@gmail.com>
2019-11-14 11:43:09 +0200

    chore: rename storybook tabs

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,10 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
+
+import addons from '@storybook/addons';
+
+setTimeout(() => {
+    addons.elements.panel['storybookjs/knobs/panel'].title = 'Properties';
+    addons.elements.panel['storybook/actions/panel'].title = 'Events';
+});
