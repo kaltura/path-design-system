f4b72cd1fa3d218988f3bb721d1d6c3d5487c71a
Eran Sakal <eran@sakalim.com>
2019-11-18 18:17:29 +0200

    docs: fix issue with storybook in monorepo not working correctly

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,57 +1,61 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
 const path = require('path');
+const glob = require('glob');
 const tsImportPluginFactory = require('ts-import-plugin');
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  resolve: {
-    extensions: [ '.ts', '.tsx', '.less', '.css']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-        loader: require.resolve('awesome-typescript-loader'),
-            options: {
-              getCustomTransformers: () => ({
-                before: [ tsImportPluginFactory({libraryName: "antd", style: true}) ]
-              }),
-            },
+module.exports = async ({config, mode}) => {
+
+  const babelLoader = config.module.rules[0];
+
+  glob.sync('./packages/*/node_modules').forEach(match => {
+    babelLoader.exclude.push(path.resolve(match));
+  });
+
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      extensions: [
+        ...config.resolve.extensions,
+        '.ts', '.tsx', '.less', '.css']
+    },
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+          options: {
+            getCustomTransformers: () => ({
+              before: [ tsImportPluginFactory({libraryName: "antd", style: true}) ]
+            }),
           },
-          {
-            loader: require.resolve('react-docgen-typescript-loader'),
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "less-loader",
-            options: { javascriptEnabled : true }
-          }
-        ],
-      }
-    ],
-  },
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    },
+    {
+      test: /\.less$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "less-loader",
+          options: { javascriptEnabled : true }
+        }
+      ],
+    }
+      ]
+    }
+  }
+  Object.assign(config, {
+
+  })
+  // Return the altered config
+  return config;
 };
