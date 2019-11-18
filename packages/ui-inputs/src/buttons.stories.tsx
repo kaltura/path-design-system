1b94835af551585c786910cb8e5b7500559f486e
Eran Sakal <eran@sakalim.com>
2019-11-18 18:18:45 +0200

    docs: expose story that demonstrate usage of icons

--- packages/ui-inputs/src/buttons.stories.tsx
+++ packages/ui-inputs/src/buttons.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from './button';
+import { Plus24Icon, SpinnerBright24Icon } from '@kaltura-path/ui-icons';
 
 export default {
   title: 'Button',
@@ -10,3 +11,14 @@ export const DefaultButton = () => <Button label={'Hi Amir!'} onClick={action('c
   DefaultButton.story = {
     title: 'Default Button'
   }
+
+export const IconA = () => <Plus24Icon />;
+IconA.story = {
+  title: 'Icon A'
+}
+
+
+export const IconB = () => <SpinnerBright24Icon spin />;
+IconA.story = {
+  title: 'Icon A'
+}
