0a72764f8c86ea8b2f89340cd1f2bdd9c82a7b6b
Eran Sakal <eran@sakalim.com>
2019-11-19 09:54:06 +0200

    chore: prioritize properties over actions tab

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,6 +1,6 @@
+import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-import '@storybook/addon-knobs/register';
 
 import addons from '@storybook/addons';
 
