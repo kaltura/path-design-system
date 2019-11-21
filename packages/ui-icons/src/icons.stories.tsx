57ab56c86fff49e889b7c5bef5011f02269aa77a
Stas <staskgt@gmail.com>
2019-11-21 13:04:26 +0200

    add icons stories

---
+++ packages/ui-icons/src/icons.stories.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import '../assets/css/styles.css';
+import * as IconsList from './index';
+
+export default {
+    title: 'Icon',
+};
+
+export const Icons = () => (
+    <main className="content">
+        <h1>Icons</h1>
+
+        <section className="icons-group">
+            <h2>16px</h2>
+            <div className="icons-group-content">
+                <IconsList.ArrowLeft16Icon className="icon"/>
+            </div>
+        </section>
+
+        <section className="icons-group">
+            <h2>24px</h2>
+            <div className="icons-group-content">
+                <IconsList.Minus24Icon className="icon"/>
+                <IconsList.Plus24Icon className="icon"/>
+                <IconsList.Redo24Icon className="icon"/>
+                <IconsList.Search24Icon className="icon"/>
+                <IconsList.SpinnerBright24Icon className="icon"/>
+                <IconsList.SpinnerDark24Icon className="icon"/>
+                <IconsList.Target24Icon className="icon"/>
+                <IconsList.Undo24Icon className="icon"/>
+                <IconsList.Upload24Icon className="icon"/>
+                <IconsList.Video24Icon className="icon"/>
+            </div>
+        </section>
+    </main>
+);
+Icons.story = {
+    title: 'Icons'
+};
