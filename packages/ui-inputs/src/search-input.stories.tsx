1cf1d8852134b89e757fde77a9415f552cde3109
Stas <staskgt@gmail.com>
2019-12-03 17:08:53 +0200

    chore: use withThemeProvider

--- packages/ui-inputs/src/search-input.stories.tsx
+++ packages/ui-inputs/src/search-input.stories.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { useRef, useState } from 'react';
-import { theme, ThemeProvider } from './theme';
 import { action } from '@storybook/addon-actions';
 import { SearchInput } from './search-input';
 import { InputElement } from './text-input';
+import { withThemeProvider } from '../storybook/with-theme-provider';
 
 export default {
     title: 'SearchInput',
+    decorators: [
+        withThemeProvider,
+    ]
 };
 
 export const DefaultInput: Story = () => {
@@ -16,27 +19,25 @@ export const DefaultInput: Story = () => {
         action('changed')(event);
         setValue(event.target.value);
     };
-    return <ThemeProvider theme={theme}>
-        <div className="row">
-            <div className="col h300">
-                <span className="label w150">Default:</span>
-                <span className="label w150">With Placeholder:</span>
-                <span className="label w150">With Error:</span>
-                <span className="label w150">Is Busy:</span>
-                <span className="label w150">Disabled:</span>
-                <span className="label w150">Uncontrolled:</span>
-            </div>
-            <div className="spacer"></div>
-            <div className="col h300 w400">
-                <SearchInput value={value} onChange={onChange}/>
-                <SearchInput value={value} placeholder="With Placeholder" onChange={onChange}/>
-                <SearchInput value={value} placeholder="With Error" hasError={true} onChange={onChange}/>
-                <SearchInput value={value} placeholder="Is Busy" isBusy={true} onChange={onChange}/>
-                <SearchInput placeholder="Disabled" disabled={true}/>
-                <SearchInput inputRef={uncontrolledInput} placeholder="Uncontrolled"/>
-            </div>
+    return <div className="row">
+        <div className="col h300">
+            <span className="label w150">Default:</span>
+            <span className="label w150">With Placeholder:</span>
+            <span className="label w150">With Error:</span>
+            <span className="label w150">Is Busy:</span>
+            <span className="label w150">Disabled:</span>
+            <span className="label w150">Uncontrolled:</span>
         </div>
-    </ThemeProvider>;
+        <div className="spacer"></div>
+        <div className="col h300 w400">
+            <SearchInput value={value} onChange={onChange}/>
+            <SearchInput value={value} placeholder="With Placeholder" onChange={onChange}/>
+            <SearchInput value={value} placeholder="With Error" hasError={true} onChange={onChange}/>
+            <SearchInput value={value} placeholder="Is Busy" isBusy={true} onChange={onChange}/>
+            <SearchInput placeholder="Disabled" disabled={true}/>
+            <SearchInput inputRef={uncontrolledInput} placeholder="Uncontrolled"/>
+        </div>
+    </div>;
 };
 
 DefaultInput.story = {
