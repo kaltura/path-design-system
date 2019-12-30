adaac391edc93029c06cd7a04c4ceaa8f0551666
Eran Sakal <eran@sakalim.com>
2019-12-30 14:53:18 +0200

    chore: use storybook 5.3 presets

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,20 @@
 const { addons } = require('@storybook/addons');
+const path = require('path');
 
 module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
+  presets: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        forkTsCheckerWebpackPluginOptions: {
+          colors: false, // disables built-in colors in logger messages
+        },
+        include: [path.resolve(__dirname, "../packages")]
+      },
+    },
+    '@storybook/preset-ant-design',
+    '@storybook/addon-docs/preset'],
   addons: [
     '@storybook/addon-docs/register',
     '@storybook/addon-knobs/register',
