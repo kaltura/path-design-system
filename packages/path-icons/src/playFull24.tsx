6fee1ad4cbec74ddd9d57794d96a0a1638dccc2e
Stas Kohut <diamond-darrell@users.noreply.github.com>
2020-03-15 16:46:33 +0200

    fix: remove broken svg scaling script (#89)

--- packages/path-icons/src/playFull24.tsx
+++ packages/path-icons/src/playFull24.tsx
@@ -3,7 +3,7 @@ import { CustomIconComponentProps, IconProps } from 'antd/lib/icon';
 import {Icon} from 'antd';
 
 const PlayFull24Svg = (props: CustomIconComponentProps) => (
-  <svg {...props}><g fillRule="evenodd"><path transform="scale(Infinity, Infinity)" d="M20.5038 13.6641l-12.3944 8.263c-.919.6127-2.1608.3643-2.7735-.5547A2 2 0 015 20.263V3.737c0-1.1045.8954-2 2-2a2 2 0 011.1094.336l12.3944 8.2629c.9191.6127 1.1675 1.8544.5547 2.7735a2 2 0 01-.5547.5547z" /></g></svg>
+  <svg {...props}><g fillRule="evenodd"><path d="M20.5038 13.6641l-12.3944 8.263c-.919.6127-2.1608.3643-2.7735-.5547A2 2 0 015 20.263V3.737c0-1.1045.8954-2 2-2a2 2 0 011.1094.336l12.3944 8.2629c.9191.6127 1.1675 1.8544.5547 2.7735a2 2 0 01-.5547.5547z" /></g></svg>
 );
 
 export const PlayFull24Icon = (props: IconProps) => {
--- packages/path-icons/src/plus16.tsx
+++ packages/path-icons/src/plus16.tsx
@@ -3,7 +3,7 @@ import { CustomIconComponentProps, IconProps } from 'antd/lib/icon';
 import {Icon} from 'antd';
 
 const Plus16Svg = (props: CustomIconComponentProps) => (
-  <svg {...props}><g fillRule="evenodd"><path transform="scale(Infinity, Infinity)" d="M8 3c.5523 0 1 .4477 1 1v3h3c.5523 0 1 .4477 1 1s-.4477 1-1 1H9v3c0 .5523-.4477 1-1 1s-1-.4477-1-1V9H4c-.5523 0-1-.4477-1-1s.4477-1 1-1h3V4c0-.5523.4477-1 1-1z" /></g></svg>
+  <svg {...props}><g fillRule="evenodd"><path d="M8 3c.5523 0 1 .4477 1 1v3h3c.5523 0 1 .4477 1 1s-.4477 1-1 1H9v3c0 .5523-.4477 1-1 1s-1-.4477-1-1V9H4c-.5523 0-1-.4477-1-1s.4477-1 1-1h3V4c0-.5523.4477-1 1-1z" /></g></svg>
 );
 
 export const Plus16Icon = (props: IconProps) => {
