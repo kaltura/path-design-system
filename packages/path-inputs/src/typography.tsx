8f58f4577c95d43c10e5d443d34f7452ae398302
Ainvoner <2538825+ainvoner@users.noreply.github.com>
2020-03-30 13:08:39 +0300

    fix: break whole words if too long PATH-461 (#105)

--- packages/path-inputs/src/typography.tsx
+++ packages/path-inputs/src/typography.tsx
@@ -47,7 +47,7 @@ export interface TypographyProps {
 
 const useStyles = createUseStyles((theme: Theme) => ({
   fontStyle :{
-    wordBreak: 'break-all',
+    wordBreak: 'break-word',
     fontStretch: 'normal',
     fontStyle: 'normal',
     letterSpacing: 'normal',
