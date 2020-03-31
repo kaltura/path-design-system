9374e6daac3043689933b3caad26dbcab3907a63
Stas Kohut <staskgt@gmail.com>
2020-03-31 17:26:27 +0300

    chore: fix type imports

--- packages/path-inputs/src/index.ts
+++ packages/path-inputs/src/index.ts
@@ -5,4 +5,4 @@ export * from './hint';
 export * from './typography';
 export * from './tag';
 export * from './text-area-input';
-export * from './dropdown-menu/dropdown-menu';
+export * from './dropdown-menu';
