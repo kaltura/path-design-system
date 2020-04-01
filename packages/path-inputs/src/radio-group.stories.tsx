873dcec0edf7993bd344dbe372de7f6fd81839c1
Stas Kohut <staskgt@gmail.com>
2020-04-01 18:21:48 +0300

    chore: add stories

--- packages/path-inputs/src/radio-group.stories.tsx
+++ packages/path-inputs/src/radio-group.stories.tsx
@@ -1,26 +1,126 @@
 import * as React from 'react';
-import { withThemeProvider } from '../storybook/with-theme-provider';
-import { withKnobs } from '@storybook/addon-knobs';
-import { RadioGroup } from './radio-group';
-import { createUseStyles } from '@kaltura-react-ui-kits/path-theming';
 import { useState } from 'react';
+import { withThemeProvider } from '../storybook/with-theme-provider';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
+import { RadioGroup } from './radio-group';
 import { RadioChangeEvent } from 'antd/es/radio';
 import { action } from '@storybook/addon-actions';
+import { createUseStyles } from '@kaltura-react-ui-kits/path-theming';
 
 const useStyles = createUseStyles({
-  'container': {
+  container: {
     padding: '50px'
-  },
+  }
 });
 
 export const Default: Story = () => {
-  const classes = useStyles();
+  const [value, setValue] = useState(1);
+  const options = [
+    { label: 'Option 1', value: 1 },
+    { label: 'Option 2', value: 2 },
+    { label: 'Option 3', value: 3 },
+  ];
+
+  const handleChange = (e: RadioChangeEvent) => {
+    setValue(e.target.value);
+    action('onChange')(e)
+  };
+
+  return (
+    <RadioGroup value={value} onChange={handleChange} options={options}/>
+  );
+};
+
+export const Change: Story = () => {
+  const [value, setValue] = useState(1);
+  const options = [
+    { label: 'Option 1', value: 1 },
+    { label: 'Option 2', value: 2 },
+    { label: 'Option 3', value: 3 },
+  ];
+
+  const handleChange = (e: RadioChangeEvent) => {
+    setValue(e.target.value);
+    action('onChange')(e)
+  };
+
+  return (
+    <RadioGroup value={value} onChange={handleChange} options={options}/>
+  );
+};
+
+Change.story = {
+  parameters: {
+    docs: {
+      storyDescription: `RadioGroup is controlled component to make it work <code>value</code> and <code>onChange</code> properties must be provided.`,
+    }
+  }
+};
+
+export const Disabled: Story = () => {
+  const [value, setValue] = useState(1);
+  const options = [
+    { label: 'Option 1', value: 1 },
+    { label: 'Option 2', value: 2 },
+    { label: 'Option 3', value: 3 },
+  ];
+
+  const handleChange = (e: RadioChangeEvent) => {
+    setValue(e.target.value);
+    action('onChange')(e)
+  };
+
+  return (
+    <RadioGroup disabled={true}
+                value={value}
+                onChange={handleChange}
+                options={options}/>
+  );
+};
+
+Disabled.story = {
+  parameters: {
+    docs: {
+      storyDescription: `To prevent a user form interacting with the component set <code>disabled</code> property to <code>true</code>.`,
+    }
+  }
+};
+
+export const DisabledOption: Story = () => {
   const [value, setValue] = useState(1);
   const options = [
     { label: 'Option 1', value: 1 },
     { label: 'Option 2', value: 2, disabled: true },
-    { label: 'Option 3 pot', value: 3 },
-    { label: 'Option 4', value: 4 },
+    { label: 'Option 3', value: 3 },
+    { label: 'Option 4', value: 4, disabled: true },
+  ];
+
+  const handleChange = (e: RadioChangeEvent) => {
+    setValue(e.target.value);
+    action('onChange')(e)
+  };
+
+  return (
+    <RadioGroup value={value} onChange={handleChange} options={options}/>
+  );
+};
+
+DisabledOption.story = {
+  parameters: {
+    docs: {
+      storyDescription: `To prevent a user form interacting with certain options provide <code>disabled</code> property for an item in the options array.`,
+    }
+  }
+};
+
+export const Workshop: Story = () => {
+  const [value, setValue] = useState(1);
+  const classes = useStyles();
+  const options = [
+    { label: 'Option 1', value: 1 },
+    { label: 'Option 2', value: 2 },
+    { label: 'Option 3', value: 3 },
+    { label: 'Option 4', value: 4, disabled: true },
   ];
 
   const handleChange = (e: RadioChangeEvent) => {
@@ -30,9 +130,21 @@ export const Default: Story = () => {
 
   return (
     <div className={classes.container}>
-      <RadioGroup value={value} onChange={handleChange} options={options}/>
+      <RadioGroup value={value}
+                  onChange={handleChange}
+                  options={options}
+                  disabled={boolean('Disabled', false)}/>
     </div>
   );
+};
+
+Workshop.story = {
+  parameters: {
+    docs: {
+      disable: true,
+      storyDescription: `An example that includes all the features to be able to test them together. It is here for internal use only and will be removed from the documentation soon.`,
+    }
+  }
 };
 
 export default {
@@ -43,6 +155,6 @@ export default {
   ],
   component: RadioGroup,
   parameters: {
-    componentSubtitle: ``,
+    componentSubtitle: `The difference from Select is that Radio is visible to the user and can facilitate the comparison of choice, which means there shouldn't be too many of them.`,
   },
 };
--- packages/path-inputs/src/radio-group.tsx
+++ packages/path-inputs/src/radio-group.tsx
@@ -8,28 +8,62 @@ import './radio-group.css';
 const { Group, Button } = Radio;
 
 export interface RadioGroupOption {
+  /**
+   * Label of the option button
+   */
   label: string;
+
+  /**
+   * Value of the option
+   */
   value: ReactText;
+
+  /**
+   * Flag that indicates disabled state of an option
+   */
   disabled?: boolean;
 }
 
 export interface RadioGroupProps {
+  /**
+   * Current value of the input
+   */
   value: ReactText;
+
+  /**
+   * Options array of the input
+   */
   options: RadioGroupOption[];
+
+  /**
+   * Change event
+   */
   onChange: (value: RadioChangeEvent) => void;
+
+  /**
+   * Flag that indicates disabled state of the input
+   */
   disabled?: boolean;
 }
 
+const prefixCls = 'path-radio';
+const optionPrefixCls = 'path-radio-button';
 
+/**
+ * Used to select a single state from multiple options
+ */
 export const RadioGroup: FunctionComponent<RadioGroupProps> = (props) => {
   const { value, options, disabled, onChange } = props;
   return (
-    <Group value={value} disabled={disabled} onChange={onChange} prefixCls="path-radio">
+    <Group value={value}
+           disabled={disabled}
+           onChange={onChange}
+           prefixCls={prefixCls}>
       {options.map(option =>
         <Button key={option.value}
                 value={option.value}
                 disabled={option.disabled}
-                prefixCls="path-radio-button">
+                prefixCls={optionPrefixCls}>
           {option.label}
         </Button>
       )}
